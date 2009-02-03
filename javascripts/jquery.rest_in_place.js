17ca4e3060a1420bf13d9b9d89ceeba2bcc144d2
Nando <fernando.garcia@the-cocktail.com>
2009-02-04 01:24:18 +0800

    Changed jQuery.ajax "type" from "put" to "post" to avoid endless requests with IE6/IE7
    Signed-off-by: janv <jan@varwig.org>

--- javascripts/jquery.rest_in_place.js
+++ javascripts/jquery.rest_in_place.js
@@ -14,7 +14,7 @@ jQuery.fn.rest_in_place = function(url, objectName, attributeName) {
 			e.html("saving...");
 			jQuery.ajax({
 				"url" : url,
-				"type" : "put",
+				"type" : "post",
 				"data" : "_method=put&"+objectName+'['+attributeName+']='+encodeURIComponent(value)+(window.rails_authenticity_token ? "&authenticity_token="+window.rails_authenticity_token : ''),
 				"success" : function(){
 					jQuery.ajax({
