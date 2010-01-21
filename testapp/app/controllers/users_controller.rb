71f8eb8fbe9471c222c4aae35d61f852a55491b1
Jan Varwig <jan@varwig.org>
2010-01-21 23:38:17 +0100

    Fix the UsersController in the testapp

--- testapp/app/controllers/users_controller.rb
+++ testapp/app/controllers/users_controller.rb
@@ -9,21 +9,23 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
     respond_to do |type|
       type.html
-      type.js {render :json => @user}
+      type.json {render :json => @user}
     end
   end
 
   def update
     @user = User.find params[:id]
     if @user.update_attributes!(params[:user])
-       flash[:notice] = 'Person was successfully updated.'
-       format.html {redirect_to( @person )}
-       format.json {render :nothing => true}
-       # format.js    # update.js.erb
+      flash[:notice] = 'Person was successfully updated.'
+      respond_to do |format|
+        format.html { redirect_to( @person )  }
+        format.json { render :nothing =>  true }
+      end
     else
-      format.html {render :action => :edit} # edit.html.erb
-      format.json {render :nothing => true}
-      format.js   {render :action => :edit} # edit.js.erb
+      respond_to do |format|
+        format.html { render :action  => :edit } # edit.html.erb
+        format.json { render :nothing =>  true }
+      end
     end
   end
 end
