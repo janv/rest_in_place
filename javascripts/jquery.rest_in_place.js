7723f3aac6e6d7d928ff5e32dcb0042864d10241
Jan Varwig <jan@varwig.org>
2010-01-24 23:27:12 +0100

    Switched to a object oriented style

--- javascripts/jquery.rest_in_place.js
+++ javascripts/jquery.rest_in_place.js
@@ -1,78 +1,81 @@
-var rest_in_place = {
-  "main" :  function() {
-    var e = this;
-    var options = rest_in_place.resolveOptionsFor(e)
+function RestInPlaceEditor(e) {
+  this.element = jQuery(e);
+  this.initOptions();
+  
+  var editor = this;
+  
+  function clickHandler() {
+    var oldValue = editor.element.html();
+    editor.element.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' + oldValue + '"></form>')
+    editor.element.find("input")[0].select();
+    editor.element.unbind('click', clickHandler)
+    editor.element.find("input").blur(function inputBlurHandler(){
+      editor.element.html(oldValue).click(clickHandler);
+    })
+    editor.element.find("form").submit(function submitHandler(){
+      jQuery.ajax({
+        "url"        : editor.url,
+        "type"       : "post",
+        "dataType"   : "text",
+        "beforeSend" : function(xhr){ xhr.setRequestHeader("Accept", "application/json"); },
+        "data"       : editor.requestData(),
+        "success"    : function saveSuccessCallback(data, textStatus){
+          jQuery.ajax({
+            "url"        : editor.url,
+            "beforeSend" : function(xhr){ xhr.setRequestHeader("Accept", "application/json"); },
+            "success"    : function loadSuccessCallback(data){
+              //jq14: data as JS object, not string.
+              if (jQuery.fn.jquery < "1.4") data = eval('(' + data + ')' );
+              editor.element.html(data[editor.objectName][editor.attributeName]);
+              editor.element.click(clickHandler);
+            }
+          });
+        }
+      });
+      editor.element.html("saving...");
+      return false;
+    })
+  }
+  editor.element.click(clickHandler);
+  
+}
 
-    function clickHandler() {
-      var oldValue = e.html();
-      e.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' + oldValue + '"></form>')
-      e.find("input")[0].select();
-      e.unbind('click', clickHandler)
-      e.find("input").blur(function inputBlurHandler(){
-        e.html(oldValue).click(clickHandler);
-      })
-      e.find("form").submit(function submitHandler(){
-        jQuery.ajax({
-          "url"        : options.url,
-          "type"       : "post",
-          "dataType"   : "text",
-          "beforeSend" : function(xhr){ xhr.setRequestHeader("Accept", "application/json"); },
-          "data"       : rest_in_place.requestData(options, e.find("input").val()),
-          "success"    : function saveSuccessCallback(data, textStatus){
-            jQuery.ajax({
-              "url"        : options.url,
-              "beforeSend" : function(xhr){ xhr.setRequestHeader("Accept", "application/json"); },
-              "success"    : function loadSuccessCallback(data){
-                //jq14: data as JS object, not string.
-                if (jQuery.fn.jquery < "1.4") data = eval('(' + data + ')' );
-                e.html(data[options.objectName][options.attributeName]);
-                e.click(clickHandler);
-              }
-            });
-          }
-        });
-        e.html("saving...");
-        return false;
-      })
-    }
-    this.click(clickHandler);
-    return this;
-  },
-
-  "resolveOptionsFor" :  function(e){
-    var url, objectName, attributeName;
+RestInPlaceEditor.prototype = {
+  initOptions : function() {
     // Try parent supplied info
-    e.parents().each(function(){
-      url           = jQuery(this).attr("data-url");
-      objectName    = jQuery(this).attr("data-object");
-      attributeName = jQuery(this).attr("data-attribute");
+    var self = this;
+    self.element.parents().each(function(){
+      self.url           = jQuery(this).attr("data-url");
+      self.objectName    = jQuery(this).attr("data-object");
+      self.attributeName = jQuery(this).attr("data-attribute");
     });
     // Try Rails-id based if parents did not explicitly supply something
-    e.parents().each(function(){
+    self.element.parents().each(function(){
       var res;
       if (res = this.id.match(/^(\w+)_(\d+)$/i)) {
-        objectName = objectName || res[1];
+        self.objectName = self.objectName || res[1];
       }
     });
     // Load own attributes (overrides all others)
-    url           = e.attr("data-url")       || url    || document.location.pathname;
-    objectName    = e.attr("data-object")    || objectName;
-    attributeName = e.attr("data-attribute") || attributeName;
-
-    return {"url"     : url,
-      "objectName"    : objectName,
-      "attributeName" : attributeName};
+    self.url           = self.element.attr("data-url")       || self.url    || document.location.pathname;
+    self.objectName    = self.element.attr("data-object")    || self.objectName;
+    self.attributeName = self.element.attr("data-attribute") || self.attributeName;
   },
   
-  "requestData" : function(options, value) {
+  requestData : function(value) {
     //jq14: data as JS object, not string.
     var data = "_method=put"
-    data += "&"+options.objectName+'['+options.attributeName+']='+encodeURIComponent(value);
+    data += "&"+this.objectName+'['+this.attributeName+']='+encodeURIComponent(this.element.find("input").val());
     if (window.rails_authenticity_token) {
       data += "&authenticity_token="+encodeURIComponent(window.rails_authenticity_token);
     }
     return data;
   }
+  
 }
 
-jQuery.fn.rest_in_place = rest_in_place.main;
+
+jQuery.fn.rest_in_place = function() {
+  this.data('restInPlaceEditor', new RestInPlaceEditor(this));
+  return this;
+}
