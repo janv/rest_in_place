ad0c5500a9fc7831a56cc800ed4ed7412532e77f
Jan Varwig <jan@varwig.org>
2010-01-24 21:02:49 +0100

    Turned the jQuery version into a proper jQuery extension

--- javascripts/jquery.rest_in_place.js
+++ javascripts/jquery.rest_in_place.js
@@ -1,15 +1,35 @@
-jQuery.fn.rest_in_place = function(url, objectName, attributeName) {
+jQuery.fn.rest_in_place = function() {
   var e = this;
+  var url, objectName, attributeName;
+  // Try parent supplied info
+  e.parents().each(function(){
+    url           = jQuery(this).attr("data-url");
+    objectName    = jQuery(this).attr("data-object");
+    attributeName = jQuery(this).attr("data-attribute");
+  });
+  // Try Rails-id based if parents did not explicitly supply something
+  e.parents().each(function(){
+    var res;
+    if (res = this.id.match(/^(\w+)_(\d+)$/i)) {
+      objectName = objectName || res[1];
+    }
+  });
+  // Load own attributes (overrides all others)
+  url           = e.attr("data-url")       || url    || document.location.pathname;
+  objectName    = e.attr("data-object")    || objectName;
+  attributeName = e.attr("data-attribute") || attributeName;
+  
   function clickFunction() {
     var oldValue = e.html();
-    e.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' + oldValue + '"></form>');
-    e.find("input")[0].select();
-    e.unbind('click');
-    e.find("input").blur(function(){
-      e.html(oldValue);
-      e.click(clickFunction);
-    })
-    e.find("form").submit(function(){
+    e.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' + oldValue + '"></form>')
+      .find("input")[0]
+        .select();
+    e.unbind('click')
+      .find("input").blur(function inputBlurHandler(){
+        e.html(oldValue)
+          .click(clickFunction);
+      })
+    e.find("form").submit(function submitHandler(){
       var value = e.find("input").val();
       e.html("saving...");
       jQuery.ajax({
@@ -17,11 +37,11 @@ jQuery.fn.rest_in_place = function(url, objectName, attributeName) {
         "type" : "post",
         "beforeSend"  : function(xhr){ xhr.setRequestHeader("Accept", "application/json"); },
         "data" : "_method=put&"+objectName+'['+attributeName+']='+encodeURIComponent(value)+(window.rails_authenticity_token ? "&authenticity_token="+encodeURIComponent(window.rails_authenticity_token) : ''),
-        "success" : function(){
+        "success" : function saveSuccessCallback(){
           jQuery.ajax({
             "url" : url,
             "beforeSend"  : function(xhr){ xhr.setRequestHeader("Accept", "application/json"); },
-            "success" : function(json){
+            "success" : function loadSuccessCallback(json){
               e.html(eval('(' + json + ')' )[objectName][attributeName]);
               e.click(clickFunction);
             }
@@ -32,25 +52,9 @@ jQuery.fn.rest_in_place = function(url, objectName, attributeName) {
     })
   }
   this.click(clickFunction);
+  return this;
 }
 
 jQuery(function(){
-  jQuery(".rest_in_place").each(function(){
-    var e = jQuery(this);
-    var url; var obj; var attrib;
-    e.parents().each(function(){
-      url    = url    || jQuery(this).attr("data-url");
-      obj    = obj    || jQuery(this).attr("data-object");
-      attrib = attrib || jQuery(this).attr("data-attribute");
-    });
-    e.parents().each(function(){
-      if (res = this.id.match(/^(\w+)_(\d+)$/i)) {
-        obj = obj || res[1];
-      }
-    });
-    url    = e.attr("data-url")       || url    || document.location.pathname;
-    obj    = e.attr("data-object")    || obj;
-    attrib = e.attr("data-attribute") || attrib;
-    e.rest_in_place(url, obj, attrib);
-  });
+  jQuery(".rest_in_place").rest_in_place();
 });
