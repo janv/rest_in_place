8c1c65d056c4f206cdaa03f8cb5da3d19d51438c
Jan Varwig <jan@varwig.org>
2010-01-25 01:18:24 +0100

    Reordered the methods in the RestInPlaceEditor.prototype

--- javascripts/jquery.rest_in_place.js
+++ javascripts/jquery.rest_in_place.js
@@ -6,6 +6,45 @@ function RestInPlaceEditor(e) {
 }
 
 RestInPlaceEditor.prototype = {
+  // Public Interface Functions //////////////////////////////////////////////
+  
+  activate : function() {
+    this.oldValue = this.element.html();    
+    this.element.unbind('click', this.clickHandler)
+    this.activateForm();
+  },
+  
+  abort : function() {
+    this.element
+      .html(this.oldValue)
+      .bind('click', {editor: this}, this.clickHandler);
+  },
+  
+  update : function() {
+    var editor = this;
+    editor.ajax({
+      "type"       : "post",
+      "dataType"   : "text",
+      "data"       : editor.requestData(),
+      "success"    : function(){
+        editor.ajax({
+          "success" : function(data){ editor.loadSuccessCallback(data) }
+        });
+      }
+    });
+    editor.element.html("saving...");
+  },
+  
+  activateForm : function() {
+    this.element.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' + this.oldValue + '"></form>');
+    this.element.find('input')[0].select();
+    this.element.find("form")
+      .bind('blur',   {editor: this}, this.inputBlurHandler)
+      .bind('submit', {editor: this}, this.submitHandler);
+  },
+  
+  // Helper Functions ////////////////////////////////////////////////////////
+  
   initOptions : function() {
     // Try parent supplied info
     var self = this;
@@ -29,7 +68,7 @@ RestInPlaceEditor.prototype = {
   
   requestData : function(value) {
     //jq14: data as JS object, not string.
-    var data = "_method=put"
+    var data = "_method=put";
     data += "&"+this.objectName+'['+this.attributeName+']='+encodeURIComponent(this.element.find("input").val());
     if (window.rails_authenticity_token) {
       data += "&authenticity_token="+encodeURIComponent(window.rails_authenticity_token);
@@ -37,53 +76,31 @@ RestInPlaceEditor.prototype = {
     return data;
   },
   
-  activateForm : function() {
-    this.element.html('<form action="javascript:void(0)" style="display:inline;"><input type="text" value="' + this.oldValue + '"></form>')
-    this.element.find("input")[0].select();    
-    this.element.find("input").bind('blur',  {editor: this}, this.inputBlurHandler);
-    this.element.find("form").bind('submit', {editor: this}, this.submitHandler)
-  },
-  
-  loadSuccessCallback : function(data) {
-    //jq14: data as JS object, not string.
-    if (jQuery.fn.jquery < "1.4") data = eval('(' + data + ')' );
-    this.element.html(data[this.objectName][this.attributeName]);
-    this.element.click(this.clickHandler);    
-  },
-  
   ajax : function(options) {
     options.url = this.url;
     options.beforeSend = function(xhr){ xhr.setRequestHeader("Accept", "application/json"); };
     return jQuery.ajax(options);
   },
+
+  // Handlers ////////////////////////////////////////////////////////////////
+  
+  loadSuccessCallback : function(data) {
+    //jq14: data as JS object, not string.
+    if (jQuery.fn.jquery < "1.4") data = eval('(' + data + ')' );
+    this.element.html(data[this.objectName][this.attributeName]);
+    this.element.bind('click', {edtor: this}, this.clickHandler);    
+  },
   
   clickHandler : function(event) {
-    var editor = event.data.editor;
-    editor.oldValue = editor.element.html();    
-    editor.element.unbind('click', editor.clickHandler)
-    editor.activateForm();
+    event.data.editor.activate();
   },
 
   inputBlurHandler : function(event) {
-    var editor   = event.data.editor;
-    editor.element
-      .html(editor.oldValue)
-      .bind('click', {editor: editor}, editor.clickHandler);
+    event.data.editor.abort();
   },
   
   submitHandler : function(event) {
-    var editor = event.data.editor;
-    editor.ajax({
-      "type"       : "post",
-      "dataType"   : "text",
-      "data"       : editor.requestData(),
-      "success"    : function(){
-        editor.ajax({
-          "success" : function(data){ editor.loadSuccessCallback(data) }
-        });
-      }
-    });
-    editor.element.html("saving...");
+    event.data.editor.update();
     return false;
   }
 }
