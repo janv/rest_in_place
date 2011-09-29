dd6c7a991e6bb77b7826a5ccf74ab231bf9c65cc
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2011-09-29 10:45:22 -0300

    Changed path to javascript files if 3.1 rails version

--- install.rb
+++ install.rb
@@ -2,7 +2,11 @@
 puts "Copying files..."
 dir = "javascripts"
 %w[jquery.rest_in_place.js mootools.rest_in_place.js rest_in_place.js].each do |js_file|
-  dest_file = File.join(::Rails.root, "public", dir, js_file)
+  dest_file = if (Rails::VERSION::STRING.to_f >= '3.1.0'.to_f)
+                File.join(::Rails.root, "vendor", "assets", dir, js_file)
+              else
+                File.join(::Rails.root, "public", dir, js_file)
+              end
   src_file = File.join(File.dirname(__FILE__) , dir, js_file)
   FileUtils.cp_r(src_file, dest_file)
 end
--- uninstall.rb
+++ uninstall.rb
@@ -2,7 +2,11 @@
 puts "Deleting files..."
 dir = "javascripts"
 %w[jquery.rest_in_place.js mootools.rest_in_place.js rest_in_place.js].each do |js_file|
-  dest_file = File.join(RAILS_ROOT, "public", dir, js_file)
+  dest_file = if (Rails::VERSION::STRING.to_f >= '3.1.0'.to_f)
+              	File.join(::Rails.root, "vendor", "assets", dir, js_file)
+              else
+                File.join(::Rails.root, "public", dir, js_file)
+              end
   FileUtils.rm(dest_file)
 end
 puts "Files deleted - Uninstallation complete!"
