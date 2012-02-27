6db74b048be88c2645b7e553a4abe63cbc214c38
Jan Varwig <jan@varwig.org>
2012-02-27 22:52:26 +0100

    Removed dependency on ActiveRecord and support spying the include_root_in_json setting from Mongoid.config if Mongoid is used.

--- lib/rest_in_place.rb
+++ lib/rest_in_place.rb
@@ -4,4 +4,14 @@ module RestInPlace
   class Engine < Rails::Engine
     # no ruby code here!
   end
+  
+  def self.include_root_in_json?
+    if defined?(Mongoid)
+      Mongoid.config.include_root_in_json
+    elsif defined?(ActiveRecord)
+      ActiveRecord::Base.include_root_in_json
+    else
+      false
+    end
+  end
 end
